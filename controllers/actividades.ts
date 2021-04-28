da25f0529bebcba028bb0f12f08c59bb323473cb
Zettsuen <61870005+Zettsuen@users.noreply.github.com>
2021-04-28 16:56:16 +0200

    Paginación de los get

--- controllers/actividades.ts
+++ controllers/actividades.ts
@@ -2,10 +2,65 @@ import { Request, Response } from 'express';
 import { json } from 'sequelize/types';
 import Actividad from '../models/actividades';
 
+/**
+ * @api {get} /api/actividades Muestra todas las actividades o el numero que se desee de actividades
+ * @apiName GetActividades
+ * @apiGroup Actividades
+ *
+ * @apiParam {Number} limit Limite de actividades.
+ * @apiParam {Number} skip Offset de actividades.
+ *
+ * @apiSuccess {Number} id Te devuelve el ID de la actividad.
+ * @apiSuccess {String} nombre Te devuelve el nombre de la actividad.
+ * @apiSuccess {DATE} fecha_inicio Te devuelve la fecha de inicio de la actividad.
+ * @apiSuccess {DATE} fecha_final Te devuelve la fecha de final de la actividad.
+ * @apiSuccess {DATE} fecha_inicio_inscripcion Te devuelve la fecha de inicio de la inscripción de la actividad.
+ * @apiSuccess {DATE} fecha_final_inscripcion Te devuelve la fecha de finalización de la inscripción de la actividad.
+ * @apiSuccess {Number} maxima_capacidad Te devuelve la capacidad maxima de usuarios de la actividad.
+ * @apiSuccess {Boolean} cancelado Te devuelve false si la actividad no esta cancelada.
+ * @apiSuccess {String} organizador Te devuelve el nombre del organizador de la actividad.
+ * @apiSuccess {DATE} fecha_creacion Te devuelve la fecha de creación de la actividad.
+ * @apiSuccess {String} herramientas Te devuelve las herramientas necesarias para participar
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     { "actividades": [
+        {
+            "id": 1,
+            "nombre": "Aumenta tu expresividad",
+            "fecha_incio": "2021-04-30T15:00:00.000Z",
+            "fecha_final": "2021-04-30T17:00:00.000Z",
+            "fecha_inicio_inscripcion": null,
+            "fecha_final_inscripcion": null,
+            "maxima_capacidad": 30,
+            "cancelado": false,
+            "organizador": "Rising Recursos Humanos",
+            "fecha_creacion": "2021-04-27T20:26:49.000Z",
+            "herramientas": "[\"aa\",\"bb\"]"
+        } ]
+ *     }
+ * @apiError {Boolean} ok Identifica si la operación ha sido incorrecta o no
+ * @apiError {String} msg Te devuelve un mensaje para el error
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 500 Internal Server Error
+ *      {
+ *          ok: false,
+ *          msg: "No existe la tabla o los campos son incorrectos"
+ *      }
+ */
+
 export const getActividades = async( req: Request , res: Response ) => {
 
     try{
-        const actividades = await Actividad.findAll();
+        const {body} = req
+        let params = {};
+        if(body.limit)
+            params.limit = body.limit
+            
+        
+        if(body.skip)
+         params.offset = body.skip
+
+        const actividades = await Actividad.findAll(params);
         
         res.json({ actividades });
     }catch(Error){
@@ -18,6 +73,56 @@ export const getActividades = async( req: Request , res: Response ) => {
 
     
 }
+
+/**
+ * @api {get} /api/actividades/:id Muestra la actividad con el :id
+ * @apiName GetActividad
+ * @apiGroup Actividades
+ *
+ * @apiParam {Number} id ID de la actividad.
+ *
+ * @apiSuccess {Number} id Te devuelve el ID de la actividad.
+ * @apiSuccess {String} nombre Te devuelve el nombre de la actividad.
+ * @apiSuccess {DATE} fecha_inicio Te devuelve la fecha de inicio de la actividad.
+ * @apiSuccess {DATE} fecha_final Te devuelve la fecha de final de la actividad.
+ * @apiSuccess {DATE} fecha_inicio_inscripcion Te devuelve la fecha de inicio de la inscripción de la actividad.
+ * @apiSuccess {DATE} fecha_final_inscripcion Te devuelve la fecha de finalización de la inscripción de la actividad.
+ * @apiSuccess {Number} maxima_capacidad Te devuelve la capacidad maxima de usuarios de la actividad.
+ * @apiSuccess {Boolean} cancelado Te devuelve false si la actividad no esta cancelada.
+ * @apiSuccess {String} organizador Te devuelve el nombre del organizador de la actividad.
+ * @apiSuccess {DATE} fecha_creacion Te devuelve la fecha de creación de la actividad.
+ * @apiSuccess {String} herramientas Te devuelve las herramientas necesarias para participar
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *       {
+ *           "id": 1,
+ *           "nombre": "Aumenta tu expresividad",
+ *           "fecha_incio": "2021-04-30T15:00:00.000Z",
+ *           "fecha_final": "2021-04-30T17:00:00.000Z",
+ *           "fecha_inicio_inscripcion": null,
+ *           "fecha_final_inscripcion": null,
+ *           "maxima_capacidad": 30,
+ *           "cancelado": false,
+ *           "organizador": "Rising Recursos Humanos",
+ *           "fecha_creacion": "2021-04-27T20:26:49.000Z",
+ *           "herramientas": "[\"aa\",\"bb\"]"
+ *       } 
+ *
+ * @apiError {Boolean} ok Identifica si la operación ha sido incorrecta o no
+ * @apiError {String} msg Te devuelve un mensaje para el error
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 500 Internal Server Error
+ *      {
+ *          ok: false,
+ *          msg: "No existe la tabla o los campos son incorrectos"
+ *      }
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 404 Not Found
+ *      {
+ *          ok: false,
+ *          msg: "No existe la actividad con el id :id"
+ *      }
+ */
 
 export const getActividad = async( req: Request , res: Response ) => {
 
--- controllers/asistentes.ts
+++ controllers/asistentes.ts
@@ -5,9 +5,53 @@ import Actividad from '../models/actividades';
 import { Sequelize as sequelize, Op } from 'sequelize';
 import { now } from 'sequelize/types/lib/utils';
 
+/**
+ * @api {get} /api/usuarios Muestra todos los usuarios o el numero que se desee de usuarios
+ * @apiName GetUsuarios
+ * @apiGroup Users
+ *
+ * @apiParam {Number} limit Limite de usuarios.
+ * @apiParam {Number} skip Offset de usuarios.
+ *
+ * @apiSuccess {Number} id Te devuelve el ID del usuario.
+ * @apiSuccess {Number} id_actividad Te devuelve el ID de la actividad del usuario.
+ * @apiSuccess {String} nombre Te devuelve el nombre del usuario.
+ * @apiSuccess {String} apellido Te devuelve el apellido del usuario.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     { "asistentes" : [
+ *              {
+ *                  "id": 13,
+ *                  "id_actividad": 1,
+ *                  "nombre": "Pantaleon",
+ *                  "apellido": "Ferrer"
+ *               }
+ *           ]
+ *     }
+ * @apiError {Boolean} ok Identifica si la operación ha sido incorrecta o no
+ * @apiError {String} msg Te devuelve un mensaje para el error
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 500 Internal Server Error
+ *      {
+ *          ok: false,
+ *          msg: "No existe la tabla o los campos son incorrectos"
+ *      }
+ */
+
 export const getUsuarios = async( req: Request , res: Response ) => {
+    
     try{
-        const asistentes = await Asistente.findAll();
+        const {body} = req
+        let params = {};
+        if(body.limit)
+            params.limit = body.limit
+            
+        
+        if(body.skip)
+         params.offset = body.skip   
+        
+
+        const asistentes = await Asistente.findAll(params)
         
         res.json({ asistentes });
     }catch{
@@ -19,6 +63,44 @@ export const getUsuarios = async( req: Request , res: Response ) => {
 
     
 }
+/**
+ * @api {get} /api/usuarios/:id Muestra el usuario con el :id
+ * @apiName GetUsuario
+ * @apiGroup Users
+ *
+ * @apiParam {Number} id ID del usuario.
+ *
+ * @apiSuccess {Number} id Te devuelve el ID del usuario.
+ * @apiSuccess {Number} id_actividad Te devuelve el ID de la actividad del usuario.
+ * @apiSuccess {String} nombre Te devuelve el nombre del usuario.
+ * @apiSuccess {String} apellido Te devuelve el apellido del usuario.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     
+ *              {
+ *                  "id": 13,
+ *                  "id_actividad": 1,
+ *                  "nombre": "Pantaleon",
+ *                  "apellido": "Ferrer"
+ *               }
+ *           
+ *     
+ * @apiError {Boolean} ok Identifica si la operación ha sido incorrecta o no
+ * @apiError {String} msg Te devuelve un mensaje para el error
+ * @apiErrorExample {json} Error-Response:
+ *  @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 404 Not Found
+ *      {
+ *          ok: false,
+ *          msg: "No existe un usuario con el id :id"
+ *      }
+ * 
+ *  HTTP/1.1 500 Internal Server Error
+ *      {
+ *          ok: false,
+ *          msg: "No existe la tabla o los campos son incorrectos"
+ *      }
+ */
 
 export const getUsuario = async( req: Request , res: Response ) => {
 
@@ -30,12 +112,54 @@ export const getUsuario = async( req: Request , res: Response ) => {
         res.json(asistente);
     } else {
         res.status(404).json({
+            ok: false,
             msg: `No existe un usuario con el id ${ id }`
         });
     }
 
 
 }
+
+/**
+ * @api {post} /api/usuarios Añade un usuario y lo mete en una actividad
+ * @apiName postUsuarios
+ * @apiGroup Users
+ *
+ * @apiParam {String} nombre Nombre del usuario
+ * @apiParam {String} apellido Apelldio del usuario
+ * @apiParam {String} herramientas Herramientas del usuario con formato "[\"Herramienta\"]"
+ *
+ * @apiSuccess {Number} id Te devuelve el ID del usuario.
+ * @apiSuccess {Number} id_actividad Te devuelve el ID de la actividad del usuario.
+ * @apiSuccess {String} nombre Te devuelve el nombre del usuario.
+ * @apiSuccess {String} apellido Te devuelve el apellido del usuario.
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     
+ *              {
+ *                  "id": 13,
+ *                  "id_actividad": 1,
+ *                  "nombre": "Pantaleon",
+ *                  "apellido": "Ferrer"
+ *               }
+ *           
+ *     
+ * @apiError {Boolean} ok Identifica si la operación ha sido incorrecta o no
+ * @apiError {String} msg Te devuelve un mensaje para el error
+ * @apiErrorExample {json} Error-Response:
+ *  @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 403 Forbidden
+ *      {
+ *          ok: false,
+ *          msg: "No se puede añadir al usuario en esta actividad"
+ *      }
+ * 
+ *  HTTP/1.1 500 Internal Server Error
+ *      {
+ *          ok: false,
+ *          msg: "Hable con el administrador"
+ *      }
+ */
 
 export const postUsuario = async( req: Request , res: Response ) => {
 
